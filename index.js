63e25c46bebfb09b36011c6769c32368fb9fb42f
Cytochrome123 <hoismail2017@gmail.com>
2021-08-21 18:12:14 +0100

    o

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const contact = require("./models/message");
 
 const Message = mongoose.model("Contact");
 
-mongoose.connect("mongodb+srv://Hud:ismail@hng.nymdr.mongodb.net/HNG001?retryWrites=true&w=majority",{useUnifiedTopology: true, useNewUrlParser:true , useCreateIndex: true , useFindAndModify:false});
+mongoose.connect("mongodb://localhost/HNG",{useUnifiedTopology: true, useNewUrlParser:true , useCreateIndex: true , useFindAndModify:false});
+
+// mongoose.connect("mongodb+srv://Hud:ismail@hng.nymdr.mongodb.net/HNG001?retryWrites=true&w=majority",{useUnifiedTopology: true, useNewUrlParser:true , useCreateIndex: true , useFindAndModify:false});
  
 mongoose.connection.on("error", console.error.bind(console, "connection error"));
 mongoose.connection.once("open", ()=>console.log("Database connected!!!"));
@@ -28,8 +30,8 @@ app.use(express.urlencoded({extended: true}))
 
 app.get("/", (req,res)=>{
     res.render("home")
-})
- 
+});
+
 // app.get("/contact" , async (req,res)=>{
 //     res.render("message")
 // })
@@ -39,25 +41,21 @@ app.get("/", (req,res)=>{
 //     await message.save()
 //     res.redirect("/")
 //     res.send(err)
-    
 // })
 
 app.post("/contact" , async(req,res)=>{
     try{
-
-
     const {fullName} = req.body
     console.log(req.body)
     const message = new Message(req.body)
     await message.save()
     res.render("success" , {fullName})
-    
-}catch(err){
-console.log(err)
-}
+    }
+    catch(err){
+    console.log(err)
+    }
 })
 app.get("/success", async(req,res)=>{
     res.render("success")
 })
-
-app.listen(process.env.PORT || 8000  , ()=> console.log("Resume"))     
+app.listen(process.env.PORT || 8000  , ()=> console.log("Resume"))
